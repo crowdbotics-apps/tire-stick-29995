e00166365eaec57d10a15475017348cc973c01cd
Crowdbotics <team@crowdbotics.com>
2021-08-20 22:59:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tire_stick_29995Identifier",
+  url: "https://tire_stick_29995Identifier.botics.co",
+  api: "https://tire_stick_29995Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
